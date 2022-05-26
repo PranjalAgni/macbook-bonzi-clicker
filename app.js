a8741737fbbe58d5440c7737d5f536dc7a753993
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-05-26 23:59:23 +0530

    :rainbow: building grouping logic

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log("Hello world 👋🌈");
+const data = ["a", "b", "c", "d", "e", "f"];
+const group = [
+  ["a", "b"],
+  ["c", "d", "e", "f"],
+];
+
+const itemsToShow = 3;
+let currentIndex = 0;
+const groupItems = (pageIndex) => {
+  const startIndex = pageIndex * itemsToShow;
+  const endIndex = startIndex + itemsToShow;
+  for (let idx = 0; idx < group.length; idx++) {
+    const currentGroup = group[idx];
+    const groupEndIndex = currentIndex + currentGroup.length;
+    console.log({ currentIndex, startIndex, endIndex });
+    if (currentIndex >= startIndex && currentIndex <= endIndex) {
+      const shiftCurrentIndex = currentIndex - startIndex;
+      const currentEndIndex = Math.min(endIndex, groupEndIndex);
+      const currentGroupedItems = currentGroup.slice(
+        shiftCurrentIndex,
+        currentEndIndex
+      );
+      console.log(currentGroupedItems);
+    }
+    currentIndex += currentGroup.length;
+  }
+};
+
+groupItems(1);
